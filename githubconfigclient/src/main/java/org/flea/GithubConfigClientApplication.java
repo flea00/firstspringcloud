ce3885d6fe44065c37b23aabe8606b4cc672a18b
flea00 <3153739@qq.com>
2019-09-18 02:36:22 +0800

    commit

---
+++ githubconfigclient/src/main/java/org/flea/GithubConfigClientApplication.java
@@ -0,0 +1,17 @@
+package org.flea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: zhaoliang
+ * @Date: 2019/9/18 2:19
+ * @Description: org.flea
+ * @version: 1.0
+ */
+@SpringBootApplication
+public class GithubConfigClientApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GithubConfigClientApplication.class, args);
+    }
+}
